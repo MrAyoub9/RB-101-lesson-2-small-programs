95d0776b9c88be539c017c5ab92f32356a8d9b42
Ayoub <fang.ayoub@gmail.com>
2023-05-07 20:42:21 -0400

    Finalize the code of loan calculator project

--- car_loan_calculator/loan_calculator.rb
+++ car_loan_calculator/loan_calculator.rb
@@ -1,136 +1,81 @@
 require 'yaml'
 MESSAGES = YAML.load_file('loan_calc_messages.yml')
 
-
-def is_number?(num)
-  arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."]
-  counter = 0
-  while counter < num.size
-    return false if !arr.include?(num[counter])
-    counter += 1
+def valid_name
+  puts MESSAGES['name']
+  name = gets.chomp
+  while !name.match?(/^[a-zA-Z]+$/)
+    puts MESSAGES['invalid_name']
+    name = gets.chomp
   end
-  true
+
+  name
 end
 
-def adjust_string(num)
-  counter = 0
-  if num.include?(".")
-    d_point = num.index(".")
-    if num.to_i == 0
-      num = "0" + num.slice(d_point..num.size)
-    end
-  else
-    while counter < num.size
-      break if num[counter].to_i > 0
-      num.delete!(num[counter])
-    end
+def valid_answer
+  puts MESSAGES['start_over']
+  answer = gets.chomp.downcase
+  answer_arr = ["yes", "no", "y", "n"]
+
+  while !answer_arr.include?(answer)
+    puts MESSAGES['invalid_answer']
+    answer = gets.chomp.downcase.strip
   end
-  num == "" ? "0" : num
+
+  answer
 end
 
-def validate_number(num)
-  if !is_number?(num)
-    return false
-  else
-    num = adjust_string(num) if num.size > 1
-    array_num = num.split("").map { |n| n.to_i.to_s if n != "." }
-    array_string = num.split("").map { |n| n if n != "." }
-    return false if array_num.count(nil) > 1
-    array_num == array_string
-  end
+def validate_number(num, over_zero)
+  return false if num.to_f == 0 && over_zero
+  num.match?(/\A\d+\.\d+\Z/) || num.match?(/\A\d+\Z/)
 end
 
-def prompt(msg, var)
+def prompt(msg, over_zero)
   puts MESSAGES[msg]
   var = gets.chomp
-  while validate_number(var) == false 
+  while validate_number(var, over_zero) == false
     puts MESSAGES['valid_number']
     var = gets.chomp
   end
-  return var
+  var
 end
-  
+
 def print_results(name, loan_amount, duration, interest_rate, monthly_pmt)
   puts <<-STR
   => Name: #{name}
   => Loan Amount: #{loan_amount} dollars
   => Loan Term: #{duration} months
-  => Loan Rate: #{interest_rate} %
-  ==> Monthly Payments : #{monthly_pmt} dollars
-  ==> Total #{duration} Payments: #{monthly_pmt.to_f*duration} dollars
-  ==> Total Interest: #{(monthly_pmt.to_f*duration) - (loan_amount.to_f)} dollars
+  => Loan Rate: #{format('%.2f', interest_rate)} %
+  ==> Monthly Payments : #{format('%.2f', monthly_pmt)} dollars
+  ==> Total #{duration} Payments: #{format('%.2f', (monthly_pmt.to_f * duration))} dollars
+  ==> Total Interest: #{format('%.2f', ((monthly_pmt.to_f * duration) - (loan_amount.to_f)))} dollars
   STR
 end
 
 def calculate_pmt(loan_amount, interest_rate, duration)
-  if interest_rate > 0
-    loan_amount * ((interest_rate) / (1 - ((1 + interest_rate)**(-duration))))
-  else
-    loan_amount / duration
-  end
+  return loan_amount / duration if interest_rate == 0
+  loan_amount * ((interest_rate) / (1 - ((1 + interest_rate)**(-duration))))
 end
 
 YEAR = 12
 
 puts MESSAGES['welcome']
 
-puts MESSAGES['name']
-name = gets.chomp
-
-# while !valid_name()
-#   puts MESSAGES['valid_name']
-#   name = gets.chomp
-# end
+name = valid_name
 
 puts "Hello, #{name}."
 
 loop do
-  loan_amount = prompt('loan_amount', loan_amount)
-
-  interest_rate = prompt('interest_rate', interest_rate)
-
-  loan_term = prompt('duration', loan_term)
+  loan_amt = prompt('loan_amount', true)
+  rate = prompt('interest_rate', false)
+  loan_term = prompt('duration', true)
 
   duration = loan_term.to_f * YEAR
-  monthly_pmt = calculate_pmt(loan_amount.to_f, interest_rate.to_f, duration)
+  monthly_rate = (rate.to_f / 100) / YEAR
+  monthly_pmt = calculate_pmt(loan_amt.to_f, monthly_rate, duration)
 
-  print_results(name, loan_amount, duration, interest_rate, monthly_pmt)
+  print_results(name, loan_amt, duration, rate, monthly_pmt)
 
-  # puts "Name: #{name}"
-  # puts "Loan Amount: #{loan_amount} dollars"
-  # puts "Loan Term: #{duration} months"
-  # puts "Loan Rate: #{interest_rate}"
-  # puts "==> Monthly Payments : #{monthly_pmt}"
-
-  # print_results(name, loan_amount, duration, interest_rate, monthly_pmt)
-
-  # puts MESSAGES['loan_amount']
-  # loan_amt = gets.chomp
-  # validate_number(loan_amt)
-
-  # puts MESSAGES['interest_rate']
-  # interest_rate = gets.chomp
-  # validate_number(interest_rate)
-
-  # puts MESSAGES['duration']
-  # loan_term = gets.chomp
-  # validate_number(loan_term)
-
-  # duration = loan_term * YEAR
-  # monthly_pmt = calculate_pmt(loan_amt, interest_rate, duration)
-
-  #create variable to store this prompt
-  # <<-STR
-  # Name: #{name}
-  # Loan Type: #{loan_type}
-  # Loan Amount: #{loan_amount} dollars
-  # Loan Term: #{duration} months
-  # Loan Rate: #{APR}
-  # ==> Monthly Payments : #{monthly_pmt}
-  # STR
-
-  puts "would you like to perform another loan calculation (Yes/No)?"
-  answer = gets.chomp.downcase
-  # validate_answer(answer)
-  break if answer == "y" || answer == "yes"
+  answer = valid_answer
+  break if answer == "n" || answer == "no"
 end
